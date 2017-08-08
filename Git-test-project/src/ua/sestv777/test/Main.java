3ba0b1be93c84cd5f768156e05bb1a826d22afab
sestv777 <sestv777@gmail.com>
2017-08-08 11:32:19 +0300

    Add !!!

--- Git-test-project/src/ua/sestv777/test/Main.java
+++ Git-test-project/src/ua/sestv777/test/Main.java
@@ -3,7 +3,7 @@ package ua.sestv777.test;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from GitHub");
+		System.out.println("Hello from GitHub!!!");
 
 	}
 
